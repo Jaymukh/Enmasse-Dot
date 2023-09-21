1fdfe421c29ace878ef30a5b3d82acbebf7fca49
dayana-edwin <dayana.edwin@tarento.com>
2023-09-21 11:19:05 +0530

    update account options

--- src/components/headercontainer/AccountOptions.tsx
+++ src/components/headercontainer/AccountOptions.tsx
@@ -1,19 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import '../../App.css';
 import * as Constants from '../../utils/constants/Constants';
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
-import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import Divider from '@mui/material/Divider';
 import { MdLogout } from 'react-icons/md';
 import { useNavigate } from 'react-router-dom';
 import { RouteConstants } from '../../constants';
 import { loggedUserState } from '../../states';
 import { useRecoilValue } from 'recoil';
 import { useUserService } from '../../services';
+import { ButtonAvatar } from '../ui/button/ButtonAvatar';
+import { Button, ButtonTheme, ButtonVariant } from '../ui/button/Button';
 
 interface AccountMenuItem {
 	key: number;
@@ -57,22 +52,49 @@ const AccountOptions: React.FC<AccountOptionsProps> = ({ handleVisiblePanel }) =
 	}
 
 	return (
-		<div>
+		<div className='account-menu'>
+			<ButtonAvatar
+				onClick={handleClick}
+				image=''
+				initial={loggedUser.initial}
+				bgColor={loggedUser.userHSL}
+				classname='account-menu-button'
+			/>
 			<div>
-				<Tooltip title="Account settings">
-					<IconButton
-						onClick={handleClick}
-						size="small"
-						sx={{ ml: 2 }}
-						aria-controls={open ? 'account-menu' : undefined}
-						aria-haspopup="true"
-						aria-expanded={open ? 'true' : undefined}
-					>
-						<Avatar sx={{ width: 30, height: 30, fontSize: 16 }}>{loggedUser.initial}</Avatar>
-					</IconButton>
-				</Tooltip>
-			</div>
-			<div>
+				{open && (
+					<div className='account-menu-dropdown py-1'>
+						<Button theme={ButtonTheme.secondary} variant={ButtonVariant.transparent} className='menu-item d-flex flex-column' onClick={(event) => handleClickMenuItem(event, 0)}>
+							<div className='me-3'>
+								{loggedUser?.img ? (
+									<img src={loggedUser?.img} alt="Avatar" className="img-fluid rounded-circle" />
+								) : (
+									<>{loggedUser.initial}</>
+								)}
+
+								<span>{loggedUser.name}</span>
+							</div>
+						</Button>
+						{Constants.accountMenuItems.map((item) => (
+							<Button theme={ButtonTheme.secondary} variant={ButtonVariant.transparent}
+								key={item.key}
+								className='menu-item d-flex'
+								onClick={(event) => handleClickMenuItem(event, item.key)}
+							>
+								<div className='me-3'>{item.icon}
+									<span>{item.text}</span>
+								</div>
+							</Button>
+						))}
+						<div className='divider'></div>
+						<Button theme={ButtonTheme.secondary} variant={ButtonVariant.transparent} className='menu-item d-flex logout' onClick={handleLogout}>
+							<MdLogout className='me-3' fontSize={22} />
+							Logout
+						</Button>
+					</div>
+				)
+				}
+			</div >
+			{/* <div>
 				<Menu
 					anchorEl={anchorEl}
 					id="account-menu"
@@ -109,8 +131,8 @@ const AccountOptions: React.FC<AccountOptionsProps> = ({ handleVisiblePanel }) =
 						Logout
 					</MenuItem>
 				</Menu>
-			</div>
-		</div>
+			</div> */}
+		</div >
 	);
 }
 
